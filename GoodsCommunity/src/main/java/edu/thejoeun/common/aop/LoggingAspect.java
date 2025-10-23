733744672e5d9057aa4e614ed3732ea84a0f94dc
Emma10003 <5emma10003@gmail.com>
2025-10-23 15:11:07 +0900

    환경 세팅 (log, email)

--- GoodsCommunity/src/main/java/edu/thejoeun/common/aop/LoggingAspect.java
+++ GoodsCommunity/src/main/java/edu/thejoeun/common/aop/LoggingAspect.java
@@ -18,16 +18,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @Component : @Bean 등록까지 함께 들어있는 어노테이션
- *              Service, Mapper, Controller 와 같은 특정 기능으로 분류할 수 없는 객체 파일 문서임을 표시하는 어노테이션
- *              개발자가 만든 파일을 SpringBoot 에서 자체적으로 관리하도록 세팅
- *
- * @Aspect : 공통 관심사가 작성된 클래스임을 명시 (AOP 동작용 클래스)
- *
- * @Slf4j : log를 찍을 수 있는 객체 생성코드 추가 (lombok 회사에서 제공하는 어노테이션)
- *
- */
+
 @Component
 @Aspect
 @Slf4j
@@ -140,7 +131,7 @@ public class LoggingAspect {
         return ip;
     }
 
-}
+}d
 /*
 advice : 끼워넣을 코드 (메서드 = 기능)
 pointCut : 실제 advice 를 적용할 joinPoint(지점)
